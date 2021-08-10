45b69e5e00615059068989bf8409c29e35b702bc
Jack830421 <81092493+Jack830421@users.noreply.github.com>
2021-08-10 20:52:53 +0800

    第一次上傳完整檔案

---
+++ player.js
@@ -0,0 +1,24 @@
+//建立物件
+let player01 = {
+    Name:"志強",
+    Basic_information:{
+        Birthday:'1994/04/21',
+        Age:'27',
+        Sex:'Male',
+        HP:100000,
+        sp:100000,
+        level:99,
+        Address:'台南市 北區 北成路',
+        EmailAddress:'jack2818947@gmail.com'
+    },
+    JOb:'Assassin',//刺客
+    Attributes:'Dark'//屬性 暗
+}
+
+console.log(player01)
+
+
+
+
+
+
